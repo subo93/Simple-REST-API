b6cd15c7a82d2169eb8816244e816a674b1afffd
subo93 <nsubodhi@gmail.com>
2018-01-16 18:54:14 +0530

    Callin 2 objects

--- src/main/java/com/mycatw/demorest/CatResource.java
+++ src/main/java/com/mycatw/demorest/CatResource.java
@@ -1,9 +1,14 @@
 package com.mycatw.demorest;
 
+import java.util.Arrays;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+//import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 @Path("mycats")
 public class CatResource {
@@ -11,13 +16,21 @@ public class CatResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-public Cat getCat(){
+public List<Cat> getCat(){
 		
 		System.out.println("getCat method calles!!");
 	Cat c=new Cat();
 	c.setName("Adaykshaka");
 	c.setPoints(100);
-	return c;
+	
+	Cat c1=new Cat();
+	c1.setName("ChinChin");
+	c1.setPoints(200);
+	
+	
+	List<Cat> cats = Arrays.asList(c,c1);
+	
+	return cats;
 }
 	
 }
